5da2de155aadd003222f7b8fe280db31602970c6
Dramacydal <PulLumBerMal@gmail.com>
2023-01-07 23:32:25 +0300

    renames

--- TranslationLibrary/Imports.cs
+++ TranslationLibrary/Imports.cs
@@ -29,49 +29,49 @@ public class Imports
     // [return: MarshalAs(UnmanagedType.Bool)]
     // public static extern bool AllocConsole();
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr Translation_GetTexts([MarshalAs(UnmanagedType.LPWStr)] string path,
         [MarshalAs(UnmanagedType.LPStr)] string encoding);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern int Translation_Save(IntPtr container, [MarshalAs(UnmanagedType.LPWStr)] string path,
         [MarshalAs(UnmanagedType.LPStr)] string encoding);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.Struct)]
     public static extern TranslationRecordInfo TranslationState_GetNextRecordInfo(IntPtr container);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern void TranslationState_Reset(IntPtr container);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern void TranslationState_Dispose(IntPtr container);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstUtf8StringMarshaller))]
     public static extern string TranslationRecord_GetSource(IntPtr record);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstUtf8StringMarshaller))]
     public static extern string TranslationRecord_GetTarget(IntPtr record);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstUtf8StringMarshaller))]
     public static extern string TranslationRecord_GetContextName(IntPtr record);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstUtf8StringMarshaller))]
     public static extern string TranslationRecord_GetContextId(IntPtr record);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstUtf8StringMarshaller))]
     public static extern string TranslationRecord_GetMeta(IntPtr record);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern void TranslationRecord_SetTarget(IntPtr record,
         [MarshalAs(UnmanagedType.LPUTF8Str)] string value);
 
-    [DllImport("esmtranslationlib.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("mwtextlib.dll", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstUtf8StringMarshaller))]
     public static extern string Translation_GetLastError();
 }
